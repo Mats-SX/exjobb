0a194ee34d49cc58e96fb5a7bea7189f5d0cf21d
Mats SX <Mats.SXz@gmail.com>
2013-05-29 15:27:58 +0200

    Initial commit with everything as-is so far. No guarantees for the source.

---
+++ programming/c/zeta_transform/src/zeta_transform.d
@@ -0,0 +1,1 @@
+zeta_transform.o: src/zeta_transform.c
---
+++ programming/c/zeta_transform/src/zeta_transform.h
@@ -0,0 +1,21 @@
+#ifndef zeta_transform_h
+#define zeta_transform_h
+
+typedef struct list_t list_t;
+typedef struct subset_t subset_t;
+
+struct subset_t {
+	int f_value;
+	list_t* nbrs;
+};
+
+struct list_t {
+	void* data;
+	list_t* next;
+};
+
+list_t* parse(FILE*, int, int);
+
+list_t* new_list(void*);
+
+#endif
