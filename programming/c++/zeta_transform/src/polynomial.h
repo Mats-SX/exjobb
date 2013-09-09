270662c8ea39c535f41879b654cd7cf66034bf05
sx <mats.sxz@gmail.com>
2013-09-09 17:04:22 +0200

    Implemented GMP types in polynomial coefficients. They should be non-leaking by now (phew). Note: propagate this to k-packing!

--- programming/c++/zeta_transform/src/polynomial.h
+++ programming/c++/zeta_transform/src/polynomial.h
@@ -1,14 +1,15 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
 
-#include <vector>
 #include <iostream>
+#include <gmp.h>
 
 class Polynomial {
 	friend std::ostream& operator<<(std::ostream&, const Polynomial&);
 	public:
 	Polynomial();
-	Polynomial(const int);
+	//Polynomial(const int);
+	~Polynomial();
 	void operator+=(const Polynomial&);
 
 	// Multiplies this polynomial with the parameter polynomial.
@@ -17,19 +18,23 @@ class Polynomial {
 
 	void operator*=(const int&);
 
+	mpz_t& operator[](const int&);
+
 	// This method will throw away all terms of higher degree.
 	// It will not raise the degree of this polynomial.
 	void raise_to_the(const int);
 	
-	void set_coeff_of_degree_to(const int, const int);
-	void add_to_coeff_of_degree(const int, const int);
+	void set_degree(const int);
+	// No longer needed since we use ref-get on []
+	//void set_coeff_of_degree_to(const int, const int);
+	//void add_to_coeff_of_degree(const int, const int);
+	//const int get_coeff_of_degree(const int) const;
 	
-	const int get_coeff_of_degree(const int) const;
-	const int get_degree() const;
+	//const int get_degree() const;
 
 	private:
 	int degree;
-	std::vector<int> coeffs;
+	mpz_t* coeffs;
 };
 
 #endif
